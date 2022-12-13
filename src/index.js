5283c71988847e569170cabe54aa71f9acba69e0
dlopeiro <dribeir4@gmail.com>
2022-12-12 22:23:34 -0300

    routes created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-const express = require('express')
+import * as express from 'express'
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  res.send('GET /')
+})
+
+app.post('/', (request, response) => {
+  response.status(201).send('POST /')
+})
+
+app.put('/', (request, response) => {
+  response.status(400).send('PUT /')
+})
+
+app.delete('/', (request, response) => {
+  response.send('DELETE /')
+})
+
+app.patch('/', (request, response) => {
+  response.send('PATCH /')
 })
 
 app.listen(3000, () => console.log('Online http://localhost:3000'))
