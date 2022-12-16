6c0a83509f866ef1fa383c367aa0b8386600186c
dlopeiro <dribeir4@gmail.com>
2022-12-15 23:29:02 -0300

    login auth with jwt created

--- src/modules/user/userController.js
+++ src/modules/user/userController.js
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 
-import { signup } from './userService' // we make it explict { signup } because there's no export default in the userService file
+import { signup, login } from './userService' // we make it explict { signup } because there's no export default in the userService file
+
+const AUTH_COOKIE_NAME = 'autorization'
 
 const router = Router()
 router.post('/signup', (request, response) => {
   try {
-    const answer = signup(request.body)
-    response.send(answer)
+    const token = signup(request.body)
+    response.cookie(AUTH_COOKIE_NAME, token).status(201).send()
   } catch (err) {
     if (err.message === 'existent_email') return response.status(400).send(err.message)
     
@@ -15,7 +17,15 @@ router.post('/signup', (request, response) => {
 })
 
 router.post('/login', (request, response) => {
-  response.send('LOGIN /')
+  try{
+    const token = login(request.body)
+    response.cookie(AUTH_COOKIE_NAME, token).status(200).send()
+  } catch (err) {
+    if (err.message === 'email_not_found' || err.message === 'incorrect_password')
+      return response.status(400).send(err.message)
+    
+    response.status(500).send()
+  }
 })
 
 export default router
--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -1,4 +1,8 @@
+import { sign } from 'jsonwebtoken'
+
 let users = []
+
+const generateAccessToken = (data) => sign(data, 'secret')
 
 const getUserByEmail = (searchEmail) => users.find((object) => object.email === searchEmail)
 
@@ -7,5 +11,13 @@ export const signup = (data) => {
     throw new Error('existent_email')
   }
   users.push(data)
-  return true
+  return generateAccessToken({ email: data.email })
+}
+
+export const login = (data) => {
+  const user = getUserByEmail(data.email)
+  if (!user) throw new Error('email_not_found')
+  if (user.password !== data.password) throw new Error('incorrect_password')
+
+  return generateAccessToken({ email: data.email })
 }
